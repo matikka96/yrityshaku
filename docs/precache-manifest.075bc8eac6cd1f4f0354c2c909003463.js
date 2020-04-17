dd6e20454de3ce8ec5883ff0360ba7fb6d8fb4d2
matikka96 <31724958+matikka96@users.noreply.github.com>
2020-04-17 12:45:28 +0300

    Hosting to Github, readme updated

---
+++ docs/precache-manifest.075bc8eac6cd1f4f0354c2c909003463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6c0b35f6f9f985f01cf59a97d8be2d",
+    "url": "/yrityshaku/index.html"
+  },
+  {
+    "revision": "d3db7e19b859611eada5",
+    "url": "/yrityshaku/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d3db7e19b859611eada5",
+    "url": "/yrityshaku/static/js/2.4894e263.chunk.js"
+  },
+  {
+    "revision": "88e77cb223281485d18962ec7c3695d9",
+    "url": "/yrityshaku/static/js/2.4894e263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef1d13c028e8732611cd",
+    "url": "/yrityshaku/static/js/main.f9b0941a.chunk.js"
+  },
+  {
+    "revision": "6ef0d88b9b356055d666",
+    "url": "/yrityshaku/static/js/runtime-main.8f6d6030.js"
+  }
+]);
