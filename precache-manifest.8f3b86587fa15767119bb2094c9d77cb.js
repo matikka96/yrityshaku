42800cb8c832c5cfcf320adb874344d86000e34d
matikka96 <matvei@MacBook-Pro.home>
2020-08-23 10:08:04 +0300

    Updates

---
+++ precache-manifest.8f3b86587fa15767119bb2094c9d77cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f06d1988c772cc3607470415945fcd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a6855940be1f230474e",
+    "url": "./static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "2a6855940be1f230474e",
+    "url": "./static/js/2.ed6c7adf.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "./static/js/2.ed6c7adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6775ebc00696b2edf284",
+    "url": "./static/js/main.4a0ffb42.chunk.js"
+  },
+  {
+    "revision": "5e1839131f85bbdce6e4",
+    "url": "./static/js/runtime-main.9f1c0880.js"
+  }
+]);
